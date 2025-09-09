e659fe21809b7f4aad8a122fcb73627f9ac6607c
¡Hola! soy Gabriela Chavez <denice.toledo00@gmail.com>
2025-09-09 10:35:12 +0000

    proyecto final

--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -54,7 +54,7 @@ function Navbar() {
             Aura
           </Typography>
 
-          {/* MENU MOBILE */}
+         
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
